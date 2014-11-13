e9f47aab0cc163f700731e77084a2970a0ec3627
Rudy Bunel <bunel.rudy@gmail.com>
2014-11-13 13:38:33 +0100

    Add system of disponible values
    The algorithm is going to look for the ordering of the disponible values
    to fill in the empty cases.
    A solution is going to be a list of index, saying for each value, to
    which case it corresponds

--- sudoku.py
+++ sudoku.py
@@ -10,23 +10,32 @@ class Sudoku:
         sudoku_grid = np.zeros((SIZE,SIZE,SIZE))
         empty_cases = []
         lines = sudoku_txt.split()
+        missing_values = [SIZE for _ in range(SIZE)]
+
         for i, line in enumerate(lines):
             for j, char in enumerate(line):
                 if char!='0':
-                    sudoku_grid[i,j,int(char)-1]  = 1
+                    idx_char = int(char)-1
+                    sudoku_grid[i,j,idx_char]  = 1
+                    missing_values[idx_char] -= 1
                 else:
                     empty_cases.append((i,j))
+        dispos = []
+        for i,count in enumerate(missing_values):
+            dispos.extend([i+1 for _ in range(count)])
         
+        self.dispos = dispos
         self.sudoku_grid = sudoku_grid
         self.empty_cases = empty_cases
         
     def __repr__(self):
-        return self.visualise_sudoku()
-        
-    def visualise_sudoku(self):
+        return Sudoku.visualise_sudoku(self.sudoku_grid)
+
+    @staticmethod
+    def visualise_sudoku(sudoku):
         human_sudoku = np.zeros((SIZE,SIZE))
         for i in range(SIZE):
-            human_sudoku=human_sudoku + (i+1) * self.sudoku_grid[...,i]
+            human_sudoku=human_sudoku + (i+1) * sudoku[...,i]
         return str(human_sudoku)
         
     @staticmethod
@@ -69,3 +78,4 @@ def load_sudokus_from_file(path_to_file):
     return sudokus
 
 sudokus = load_sudokus_from_file('easy50.txt')
+sudo = sudokus[0]

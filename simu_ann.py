6a2e4293429a7fe461f3acc70d062ff30efaaa80
Rudy Bunel <bunel.rudy@gmail.com>
2014-11-27 19:26:11 +0100

    Add a probabilistic method to choose which elements to swipe
    Greatly improve the convergence speed

--- simu_ann.py
+++ simu_ann.py
@@ -1,65 +1,46 @@
 from sudoku import Sudoku, load_sudokus_from_file
 import random
 import math
-import pdb
+
 
 sudokus = load_sudokus_from_file('easy50.txt')
-sudo = sudokus[0]
+sudo = sudokus[1]
 
 def solve_sudoku(sudoku):
     # Generate initial state
-    solution = [elt  for line in sudoku.available_for_line for elt in line]
-    sudoku.fill_in_state(solution)
-    energy = sudoku.count_constraint_violation()
-    
-    best_solution = solution[:]
+    energy = sudoku.constraints_violation.sum()
     best_energy = energy
+    best_solution = sudoku.sudoku_grid.copy()
+
     temperature = 1
     i =0
     try:
         while True:
             i = i+1
-            new_sol = get_new_solution(solution, sudoku)
-
-            sudoku.fill_in_state(new_sol)
-            new_energy = sudoku.count_constraint_violation()
+            new_solution, constraints_violation = sudoku.get_new_solution()
+            new_energy = constraints_violation.sum()
 
             if should_do_transition(energy, new_energy, temperature):
                 energy = new_energy
-                solution = new_sol
-        
+                sudoku.sudoku_grid = new_solution
+                sudoku.constraints_violation = constraints_violation
+
                 if energy < best_energy:
                     best_energy = energy
-                    best_solution = solution[:]
-        
-            if best_energy == 0:
-                break
+                    best_solution = new_solution.copy()
+                if energy == 0:
+                    break
+
             if i % 1000==0:
                 print i, temperature, best_energy
 
             temperature = temperature * 0.99999
+
     except KeyboardInterrupt:
         pass
 
-    sudoku.fill_in_state(best_solution)
-
     sudoku.show_filled()
 
-def get_new_solution(solution, sudoku):
-    '''
-    Randomly permut two elements of the solution
-    This way, the two configuration should be close in terms of energy
-    '''
-    line = random.randint(0,sudoku.size-1)
-
-    cases = [idx  for (idx, case) in enumerate(sudoku.empty_cases) if case[0]==line]
-
-    a = random.randint(0, len(cases) -1)
-    b = random.randint(0, len(cases) -1)
-
-    solution[cases[a]], solution[cases[b]] = solution[cases[b]], solution[cases[a]]
-
-    return solution
 
 def should_do_transition(energy, new_energy, temperature):
     if new_energy < energy:
--- sudoku.py
+++ sudoku.py
@@ -1,89 +1,84 @@
 import numpy as np
-import pdb
+import random
 
 SMALL = 3
 SIZE = 9
 
 class Sudoku:
-     
+
     def __init__(self, sudoku_txt):
-        sudoku_grid = np.zeros((SIZE,SIZE,SIZE))
+        sudoku_grid = np.zeros((SIZE,SIZE))
         empty_cases = []
         lines = sudoku_txt.split()
-        missing_values = [SIZE for _ in range(SIZE)]
 
         available_for_line = []
 
+        # Read the sudoku
         for i, line in enumerate(lines):
             line_possibility = set(range(1,SIZE+1))
             for j, char in enumerate(line):
                 if char!='0':
                     line_possibility.discard(int(char))
-                    idx_char = int(char)-1
-                    sudoku_grid[i,j,idx_char]  = 1
-                    missing_values[idx_char] -= 1
+                    sudoku_grid[i,j]  = int(char)
                 else:
                     empty_cases.append((i,j))
             available_for_line.append(line_possibility)
-        dispos = []
-        for i,count in enumerate(missing_values):
-            dispos.extend([i+1 for _ in range(count)])
-        
-        assert len(dispos) == len(empty_cases)
+
+        # Parameters of the problem
         self.size = SIZE
-        self.dispos = dispos
         self.sudoku_grid_empty = np.copy(sudoku_grid)
-        self.sudoku_grid = sudoku_grid
         self.empty_cases = empty_cases
-        self.available_for_line = available_for_line
-        
+
+        # Generate initial solution
+        for i,line in enumerate(available_for_line):
+            for j in range(SIZE):
+                if sudoku_grid[i,j] == 0:
+                    sudoku_grid[i,j] = line.pop()
+            assert len(line)==0
+        self.sudoku_grid = sudoku_grid
+
+        # Get numbers of constraint by elements
+        self.constraints_violation = self.count_constraint_violation(self.sudoku_grid)
+
+
+
     def __repr__(self):
-        return Sudoku.visualise_sudoku(self.sudoku_grid_empty)
+        return str(self.sudoku_grid_empty)
 
     def show_filled(self):
-        print Sudoku.visualise_sudoku(self.sudoku_grid)
+        print self.sudoku_grid
 
-    @staticmethod
-    def visualise_sudoku(sudoku):
-        human_sudoku = np.zeros((SIZE,SIZE))
-        for i in range(SIZE):
-            human_sudoku=human_sudoku + (i+1) * sudoku[...,i]
-        return str(human_sudoku)
-        
-    def count_constraint_violation(self):
-        sudoku = self.sudoku_grid
-        nb_constraint = 0
-        obj = np.ones((1,SIZE))
+    def count_constraint_violation(self, solution):
+        constraints_violation = np.zeros((len(self.empty_cases),1))
 
-        # Rows constraint
-        for i in range(SIZE):
-            # np.sum(sudo[i,...], axis=0) gives us an array of the number of times
-            # a number is represented in the row i
-            nb_constraint += np.sum(np.sum(sudoku[i,...], axis=0)!=obj)
+        for k, (i, j)  in enumerate(self.empty_cases):
+            a = SMALL* (i / SMALL)
+            b = SMALL* (j / SMALL)
+            value = solution[i,j]
+            constraints_violation[k] = np.sum(solution[i,:]==value) + \
+                                       np.sum(solution[:,j]==value) + \
+                                       np.sum(solution[a:a+3,b:b+3]==value) - 3
+            # The -3 is there because there should be a value everytime
+            # -> the one we're considering
 
-        # Columns constraint
-        for j in range(SIZE):
-            # np.sum(sudo[...,j,...], axis=0) gives us an array of the number of times
-            # a number is represented in the column j         
-            nb_constraint += np.sum(np.sum(sudoku[...,j,...], axis=0)!=obj)
 
-        # Box constraint
-        for k in range(SIZE):
-            a = k / SMALL
-            b = k - a * SMALL
-            # np.sum(sudo[a:a+3,b:b+3...], axis=(0,1)) gives us an array of the number 
-            # of times a number is represented in the box of index a,b
-            nb_constraint += np.sum(np.sum(sudoku[a:a+3,b:b+3,...], axis=(0,1)) != obj)
+        return constraints_violation
 
-        return nb_constraint
-    
-    def fill_in_state(self, state):
-        assert len(state) == len(self.dispos), "%s state != %s dispos" % (len(state),len(self.dispos))
+    def get_new_solution(self):
+        solution = np.copy(self.sudoku_grid)
+        probabilities = np.exp(self.constraints_violation).cumsum()
 
-        self.sudoku_grid = np.copy(self.sudoku_grid_empty)
-        for value, pos in zip(state, self.empty_cases):
-            row, col = pos
-            self.sudoku_grid[row, col, value-1] = 1
+        mult = probabilities[-1]
+        pos1 = probabilities.searchsorted(mult*random.random())
+        pos2 = probabilities.searchsorted(mult*random.random())
+
+        idx1 = self.empty_cases[pos1]
+        idx2 = self.empty_cases[pos2]
+
+        solution[idx1], solution[idx2] = solution[idx2], solution[idx1]
+
+        return solution, self.count_constraint_violation(solution)
+
 
 def load_sudokus_from_file(path_to_file):
     with open(path_to_file, 'r') as sudoku_file:
